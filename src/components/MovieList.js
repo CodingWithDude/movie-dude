cd6bdb3b77d2bc6ec22acbbfe27a90124c99ad67
CodingWithDude <codingwithdude@gmail.com>
2022-09-29 19:37:02 -0700

    Added MUI Card component to MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,20 +1,57 @@
-import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Box } from "@mui/material";
 
 const MovieList = ({ movies }) => {
   return (
-    <>
-      {movies &&
-        movies.map((movie, index) => (
-          <div key={index}>
-            {movie.poster_path !== null && (
-              <img
-                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                alt="movie"
-              />
-            )}
-          </div>
-        ))}
-    </>
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          paddingBottom: "18px",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1">
+          Heading
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "18px",
+          justifyContent: "center",
+        }}
+      >
+        {movies &&
+          movies.map((movie, index) => (
+            <Card sx={{ width: 345 }} key={index}>
+              <CardActionArea>
+                {movie.poster_path !== null && (
+                  <CardMedia
+                    component="img"
+                    height="fit-content"
+                    image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                    alt="movie"
+                  />
+                )}
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {movie.title !== null && movie.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {movie.overview !== null && movie.overview}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+      </Box>
+    </Box>
   );
 };
 
